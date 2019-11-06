03a61a28a3a74e1a322f9378510662c816552692
Robby <robby-olson11@gmail.com>
2019-11-05 21:29:18 -0800

    calculator function works... by calculating as a string! =]

--- src/App.js
+++ src/App.js
@@ -30,15 +30,15 @@ function App() {
     if (x === 'C') {
       display = 0;
     }
+    if (x === '=') {
+      function evalString(display) {
+        return new Function('return ' + display)();
+      }
+      display = evalString(displayState);
+    }
     console.log(display);
     setDisplayState(display);
   };
-
-  // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
-  // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
-  // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
-  // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
-  // Don't forget to pass the functions (and any additional data needed) to the components as props
 
   return (
     <div className='container'>
